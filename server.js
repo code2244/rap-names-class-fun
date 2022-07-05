79e094c13b6b1ea4eaff57d27fc983ae9c30e5df
Ricardo Hernandez <rmhm.photography@gmail.com>
2022-07-04 22:06:39 -0400

    Fixed cors errors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express') //access to express
 const app = express() 
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
 const rappers = {
     '21 savage':{
